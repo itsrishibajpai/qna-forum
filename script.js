40f2955b508e87429e1515120b822ea0819d2446
Rishi Bajpai <rishibajpai@Rishis-MacBook-Air.local>
2021-06-18 16:37:33 +0530

    final

--- script.js
+++
@@ -1,8 +0,0 @@
-function openCity(cityName) {
-    var i;
-    var x = document.getElementsByClassName("city");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById(cityName).style.display = "block";
-  }
